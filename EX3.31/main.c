6e52652238141c344f9105c324b25e6daf64f66e
Jubril Abdulsalam <abdulsalamjubr3@gmail.com>
2024-06-06 06:06:31 -0700

    completed chapter 3 exercises

--- EX3.31/main.c
+++ EX3.31/main.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
 
 int main(void) {
+    int x = 5;
+    int y = 8;
 
+    // TASK A
+    printf("Task A:\n");
+    if ( y == 8 ) {
+        if ( x == 5 ) {
+            puts( "@@@@@" );
+            puts( "$$$$$" );
+            puts( "&&&&&" );
+        }
+    }
+
+    // TASK B
+    printf("Task B:\n");
+    if ( y == 8 ) {
+        if ( x == 5 ) {
+            puts( "@@@@@" );
+        }
+    }
+
+    // TASK C
+    printf("Task C:\n");
+    if ( y == 8 ) {
+        if ( x == 5 ) {
+            puts( "@@@@@" );
+            puts( "&&&&&" );
+        }
+    }
+
+    y = 7;
+
+    // TASK D
+    printf("Task D:\n");
+    if ( y == 7 ) {
+        if ( x == 5 ) {
+            puts( "#####" );
+            puts( "$$$$$" );
+            puts( "&&&&&" ); 
+        }
+    }
 }
